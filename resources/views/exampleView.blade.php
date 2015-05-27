2dacdab8a136ad4da3a095749ec0891583efc67c
fen <fen@desmart.com>
2015-05-27 23:47:15 +0200

    Literówka.

--- resources/views/exampleView.blade.php
+++ resources/views/exampleView.blade.php
@@ -36,7 +36,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Look, Mom, no cotroller!</div>
+				<div class="title">Look, Mom, no controller!</div>
 			</div>
 		</div>
 	</body>
