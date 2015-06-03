e99610f75c0c9dbee598477a5af0d7750520bbb5
fen <fen@desmart.com>
2015-06-03 22:53:36 +0200

    Widok formularza tworzenia posta.

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,9 @@
+<h1>New post</h1>
+
+<form action="post">
+    <input type="text" name="title" placeholder="Title" />
+    <br/>
+    <textarea name="post_content" cols="100" rows="15" placeholder="Content"></textarea>
+    <br/>
+    <input type="submit" value="Save" />
+</form>
