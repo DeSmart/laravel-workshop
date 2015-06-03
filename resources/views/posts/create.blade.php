fb45a5a3d3db79bcee76b58ea1ce6d1025572c05
fen <fen@desmart.com>
2015-06-03 23:02:46 +0200

    Fix formularza dodawania posta.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,7 @@
 <h1>New post</h1>
 
-<form action="post">
+<form method="post">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <input type="text" name="title" placeholder="Title" />
     <br/>
     <textarea name="post_content" cols="100" rows="15" placeholder="Content"></textarea>
