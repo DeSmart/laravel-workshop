a60014acbe796980e356b352c75f404b47b898bc
fen <fen@desmart.com>
2015-06-04 21:38:53 +0200

    Przebudowa widoku formularza dodawania postu.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,10 +1,22 @@
-<h1>New post</h1>
+@extends('app')
 
-<form method="post">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <input type="text" name="title" placeholder="Title" />
-    <br/>
-    <textarea name="post_content" cols="100" rows="15" placeholder="Content"></textarea>
-    <br/>
-    <input type="submit" value="Save" />
-</form>
+@section('content')
+    <h1>New post</h1>
+
+    <form method="post">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" name="title" id="title" />
+        </div>
+        <div class="form-group">
+            <label for="post_content">Content</label>
+            <br/>
+            <textarea name="post_content" id="post_content" rows="15" class="form-control"></textarea>
+        </div>
+        <br/>
+        <input type="submit" value="Save" class="btn btn-success" />
+    </form>
+@endsection
+
+@include('css')
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <h1>Posts</h1>
 
+    <a class="btn btn-primary" href="/posts/create">New Post</a>
+
     @foreach ($posts as $post)
         <div class="well">
             <h3>{{ $post->title }}</h3>
