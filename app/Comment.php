301c99eec3984386ff2e50231a1d6bb0e4ded206
fen <fen@desmart.com>
2015-06-04 21:20:08 +0200

    Dodanie relacji do modelu Post.

--- app/Comment.php
+++ app/Comment.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model {
+class Comment extends Model
+{
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'comments';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'comments';
 
     public $timestamps = false;
 
--- app/Post.php
+++ app/Post.php
@@ -2,15 +2,21 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model {
+class Post extends Model
+{
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'posts';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
 
     public $timestamps = false;
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'id_post');
+    }
 
 }
